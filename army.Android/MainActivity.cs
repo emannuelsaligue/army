727546e2939ad9533fc3a157c60efec9b5cd6135
emannuel <emannuelsaligue@hotmail.com>
2018-07-04 04:53:46 +0800

    json
    adding firebaseJson

--- army.Android/MainActivity.cs
+++ army.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Com.OneSignal;
 
 namespace army.Droid
 {
@@ -18,7 +19,9 @@ namespace army.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
-
+            OneSignal.Current.StartInit("e255d74f-80cc-4b68-a343-b608488dd7c6")
+                 .EndInit();
+            
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
